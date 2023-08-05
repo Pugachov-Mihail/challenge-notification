de070dc7d95f9ae368496620d0fff0c48e496889
GreeN <xd383727@gamil.com>
2023-08-05 23:41:51 +0300

    fix config

--- config/confi_db.py
+++ config/confi_db.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from notification_api.config.settings import MONGO_DB_URL
+from config.settings import MONGO_DB_URL
 
 
 conn = AsyncIOMotorClient(MONGO_DB_URL)
--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import requests
 from celery import Celery
 
+from config import confi_db
 from config.settings import REDIS_DB_URL
+from models.model_notification import notifications_entity
+from shemas.notification import Notification
 
 app = Celery(__name__, broker=REDIS_DB_URL)
 
 
 def request_api(url: str):
-    request = requests.get(url).json()
-    return [item for item in request]
+    try:
+        request = requests.get(url).json()
+        return request
+    except requests.exceptions.ConnectionError:
+        raise requests.exceptions.ConnectionError("Ошибка подключения")
 
 
-def _create_data_in_mongodb():
-     res = request_api("localhost:8080/get_notification")
-     confi_db.db.values.insert_many([_data_for_mongo(i) for i in res])
-     list_data = confi_db.db.values.find({})
-     return notifications_entity(list_data)
+def create_data_in_mongodb():
+    res = request_api("http://127.0.0.1:8000/api-notification/get_notification")
+    confi_db.db.values.insert_many(res)
 
 
 def _data_for_mongo(item: Notification):
-     return {
-         "day_week": item.day_week,
-         "periodicity": item.periodicity,
-         "period": item.period,
-         "time_start": str(item.time_start),
-         "time_end": str(item.time_end),
-         "challenge": {
-             "id": str(item.challenge.id),
-             "user_id": str(item.challenge.user_id)
-         }
-     }
+    print(item)
+    return {
+        "day_week": item.day_week,
+        "periodicity": item.periodicity,
+        "period": item.period,
+        "time_start": str(item.time_start),
+        "time_end": str(item.time_end),
+        "challenge": {
+            "id": str(item.challenge.id),
+            "user_id": str(item.challenge.user_id)
+        }
+    }
+
+
+create_data_in_mongodb()
